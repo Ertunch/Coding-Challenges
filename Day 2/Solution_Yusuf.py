c39809b016a2bc064ff0b93fc05723370d708828
Yusuf Baktir, Ph.D <33700432+ybaktir@users.noreply.github.com>
2019-03-01 21:02:38 -0600

    Create Solution_Yusuf.py

---
+++ Day 2/Solution_Yusuf.py
@@ -0,0 +1,25 @@
+#Solution 1
+a = [3,2,1]
+outcome = []
+for i in range(len(a)):
+    b=a.copy()
+    result = 1
+    del b[i]
+    for j in b:
+        result = result * j
+    outcome.append(result)
+    
+    
+    
+#Solution 2
+a = [3, 2, 1]
+e =0
+b = []
+r=1
+while e < len(a):
+    for i,j in enumerate(a):
+        if i != e:
+            r=r*a[i]
+    b.append(r)
+    r = 1
+    e=e+1
