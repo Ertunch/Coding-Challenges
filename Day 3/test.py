7ca7536a95bbdd8e60a62285588115bf869c0cd1
sukruc <sukrucsezer@outlook.com>
2019-03-03 10:31:52 -0800

    added test cases

--- Day 3/test.py
+++ Day 3/test.py
@@ -1,6 +1,12 @@
-import sys
-sys.path.append('./Day 3')
+"""Your algorithms timing can be tested here.
+Use _timing_wrapper() function to create a timed instance of your function
+and use numbered test cases as input."""
 
+import sys
+try:
+    sys.path.append('./Day 3')
+except:
+    pass
 from funcs import *
 from test_cases import *
 from utils import _timing_wrapper
@@ -16,6 +22,7 @@ test(test_case_6)
 test(test_case_7)
 test(test_case_8)
 test(test_case_9)
+
 test = _timing_wrapper(looping)
 test(test_case_1)
 test(test_case_2)
