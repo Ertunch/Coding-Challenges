68c36674825872692ddc7098bab9effa8867f82d
sukruc <sukrucsezer@outlook.com>
2019-03-03 10:39:55 -0800

    added comments

--- Day 3/funcs.py
+++ Day 3/funcs.py
@@ -1,21 +1,22 @@
 def looping(x):
+    """Finds the smallest positive integer that is not included in the array."""
     a = x.copy()
     a.sort()
     i = 0
     missing = 0
     while i < len(a) and a[i] <= 0:
-        i += 1
+        i += 1  # Loop through negative integers, do nothing
     while i < len(a):
         if i == len(a) - 1:
-            missing = a[i] + 1
+            missing = a[i] + 1  # Return next positive integer if no missing found
             i += 1
             break
-        elif a[i] == a[i+1]:
+        elif a[i] == a[i+1]:  # If element is duplicated, continue
             i += 1
-        elif a[i] == a[i+1] - 1:
+        elif a[i] == a[i+1] - 1:  # If next element in array is a continuation, continue
             i += 1
         else:
-            missing = a[i] + 1
+            missing = a[i] + 1  # If next element in array is not a continuation, return a[i]+1
             i += 1
             break
     if a[i - 1] <= 0:
@@ -28,6 +29,8 @@ def gener(a):
 
 
 def negate(a):
+    """Finds the smallest positive integer that is not included in the array.
+    ELiminates negative integers prior to sorting the array."""
     i = 0
     positives = []
     while i < len(a):
