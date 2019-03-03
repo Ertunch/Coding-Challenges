b5ac525e7d89c38744eaa98245f4eaf9f3cce9f6
Yusuf Baktir, Ph.D <33700432+ybaktir@users.noreply.github.com>
2019-03-02 23:30:18 -0600

    Update Solution3_Yusuf.py

--- Day 3/Solution3_Yusuf.py
+++ Day 3/Solution3_Yusuf.py
@@ -1,6 +1,12 @@
 a = [3, 4, -1, 1]
 a = [1, 2, 0]
+#Solution 1
 for i in range(1,len(a)+1):
     if i not in a:
         print(i)
         break
+
+b=(n**2 for n in range(12))
+
+#Solution 2
+next(i for i in range(1,len(a)+1) if i not in a)
