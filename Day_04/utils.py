fd913edf17177d6ca95df9be504d4ba803166a4e
sukruc <sukrucsezer@outlook.com>
2019-03-03 23:50:33 -0800

    removed unnecessary comment lines

--- Day_04/utils.py
+++ Day_04/utils.py
@@ -1,17 +1,6 @@
 from datetime import datetime
 import numpy as np
 
-# def _timing_wrapper(func):
-#     def test(func, array, shuffle=True):
-#         test_array = array.copy()
-#         if shuffle:
-#             np.random.shuffle(test_array)
-#         tic = datetime.now()
-#         func(test_array)
-#         toc = datetime.now()
-#         print('Function executed : %.5f seconds' % (toc-tic).total_seconds())
-#     return test
-
 def _timing_wrapper(func, *args):
     """Embeds function for a timing operation.
     Usage:
