915c2db2f620c99431bcf64db3b4f8b0eb2768f5
sukruc <sukrucsezer@outlook.com>
2019-03-07 03:16:27 -0800

    updated test and profiling tools

--- Day 5/utils.py
+++ Day 5/utils.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 import numpy as np
 
-def _timing_wrapper(func, *args):
+
+def _timing_wrapper(func, *args, **kwargs):
     """Embeds function for a timing operation.
     Usage:
     >>>test = _timing_wrapper(func)
-    >>>test(*func_args)
+    >>>test(*func_args, **func_kwargs)
     """
-    def timing(*args):
+    def timing(*args, **kwargs):
         # print(args)
         then = datetime.now()
-        func(*args)
+        func(*args, **kwargs)
         now = datetime.now()
         print('Function executed : %.5f seconds' % (now-then).total_seconds())
     return timing
